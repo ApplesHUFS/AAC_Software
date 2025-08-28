f018d4124ef188450b3fc5b311d34131e3c18d11
YounghwanShin <yong060309@gmail.com>
2025-08-28 15:19:45 +0900

    chore: cosine similarities 사용 방식 수정

--- src/private/cluster_similarity_calculator.py
+++ src/private/cluster_similarity_calculator.py
@@ -72,8 +72,13 @@ class ClusterSimilarityCalculator:
             embeddings1 = self.similarity_model.encode(topics1, convert_to_tensor=True)
             embeddings2 = self.similarity_model.encode(topics2, convert_to_tensor=True)
 
-            similarities = torch.mm(embeddings1, embeddings2.T)
-            similarities = (similarities + 1) / 2  # -1~1을 0~1로 변환
+            similarities = torch.nn.functional.cosine_similarity(
+            embeddings1.unsqueeze(1),  # (topics1, 1, dim)
+            embeddings2.unsqueeze(0),  # (1, topics2, dim)
+            dim=-1
+            )
+            similarities = similarities.cpu().numpy()  # (len(topics1), len(topics2))
+
 
             return torch.clamp(similarities, 0.0, 1.0).cpu().numpy()
         except Exception as e:
